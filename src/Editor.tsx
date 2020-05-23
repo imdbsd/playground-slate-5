f09583c28c87f4fad96243bdb9533976a9f70954
I Made Budi Surya Darma <budisuryadarma0407@gmail.com>
2020-05-23 14:59:51 +0800

    init Editor

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react'
+import { createEditor, Node } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
 
 const Editor = () => {
   console.log('editor comp')
-  return <section>editor</section>
+  const [editorValue, setEditorValue] = React.useState<Node[]>([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+  const handleChangeEditorValue = (value: Node[]) => {
+    setEditorValue(value)
+  }
+  const editor = React.useMemo(() => withReact(createEditor()), [])
+
+  return (
+    <Slate
+      editor={editor}
+      value={editorValue}
+      onChange={handleChangeEditorValue}
+    >
+      <Editable />
+    </Slate>
+  )
 }
 
 export default Editor
